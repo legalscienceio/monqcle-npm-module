27eabab13c5886df35c83427ebbdf8166c6e0d7b
Mark Rose <mark@Marks-MacBook-Pro.local>
2017-05-22 15:38:13 -0400

    flushing out the rest of the functions

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-var api_host = "http://test.monqcle.com/";
-var site_id = "56e805b9d6c9e75c1ac8cb12";
+var fs = require('fs');
+var config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+var api_host = config.host;
+var site_id = config.site;
 var _ = require('lodash');
 request = require('request-json');
 var client = request.createClient(api_host);
@@ -9,155 +11,306 @@ var Promise = require('promise');
 
 var exports = module.exports = {
    
-  dataset: function (name, display) {
-      return exports.dataset_promise(name, display)
-        //.then(exports.parseResult)
-        //.then(null, this.retryErrors)
-        //.nodeify(callback)
-        .then(function(d){ 
-          //console.log(d);
-          return d;
-        });
-  },
-  dataset_siteitem: function(name, display, callback) {      
-     if(display && display != ''){
-          url = 'siteitem/' + name + '/get_by_dataset?site=' + site_id + '&display=' + display;
-     } else {
-          url = 'siteitem/' + name + '/get_by_dataset?site=' + site_id;		
-     }
-     
-     var promise = new Promise(function (resolve, reject) {
-      client.get(url, function (err, res) {
-        if (err) reject(err);
-        else resolve(res);
-      });
-    });
-    
-    if(typeof callback != 'undefined'){
-        return Promise.resolve(promise)
+    assets: function (callback) {
+      
+      url = 'site/' + site_id + '/zget';
+      if(typeof callback != 'undefined'){
+        return Promise.resolve(exports.get_promise(url))
         .nodeify(callback);
-    } else {
-        return Promise.resolve(promise);
-    }
-  },
-  /**
-  * Escape special characters in the given string of html.
-  *
-  * @param  {String} name
-  * @param  {String} display
-  * @return {String}
-  */
-  dataset_nodeify: function (name, display, callback) {
-      return exports.dataset_promise(name, display)
-        //.then(exports.parseResult)
-        //.then(null, this.retryErrors)
-        .nodeify(callback)
-  },
-  parseResult: function(result) {
-      //console.log(result);
-      return result;
-  },
-  retryErrors: function(errors) {
-      console.error(errors);
+      } else {
+          return exports.get_promise(url)
+          //return exports.dataset_promise(name, display)
+            //.then(exports.parseResult)
+            //.then(null, this.retryErrors)
+            //.nodeify(callback)
+            .then(function(d){ 
+              //console.log(d);
+              return d;
+            });
+      }
+    },
+    dataset: function (name, display, callback) {
       
-  },    
-    
-  dataset_promise: function(name, display) {      
-     if(display && display != ''){
+      if(display && display != ''){
           url = 'siteitem/' + name + '/get_by_dataset?site=' + site_id + '&display=' + display;
-     } else {
+      } else {
           url = 'siteitem/' + name + '/get_by_dataset?site=' + site_id;		
-     }
+      }
+      if(typeof callback != 'undefined'){
+        return Promise.resolve(exports.get_promise(url))
+        .nodeify(callback);
+      } else {
+          return exports.get_promise(url)
+          //return exports.dataset_promise(name, display)
+            //.then(exports.parseResult)
+            //.then(null, this.retryErrors)
+            //.nodeify(callback)
+            .then(function(d){ 
+              //console.log(d);
+              return d;
+            });
+      }
+    },
+    
+    questions: function (name, is_legacy, callback) {
+      
+      if(is_legacy && is_legacy != '' && (is_legacy == true || is_legacy == "true" )){
+          url = 'questions/' + name + '?use_lawatlas=true';
+      } else {
+          url= 'api/v3.0/questions/' + name;	
+      }
+      if(typeof callback != 'undefined'){
+        return Promise.resolve(exports.get_promise(url))
+        .nodeify(callback);
+      } else {
+          return exports.get_promise(url)
+          //return exports.dataset_promise(name, display)
+            //.then(exports.parseResult)
+            //.then(null, this.retryErrors)
+            //.nodeify(callback)
+            .then(function(d){ 
+              //console.log(d);
+              return d;
+            });
+      }
+    },
+    
+    query: function (name, config, params, callback) {
+      
+        //is_legacy, state, jurisdictions, start_year, end_year
+        url = "api/v3.0/dataset/query/" + name + "?site_key=" + site_id;
+        if ("use_lawatlas" in config) {
+            url= url + '&use_lawatlas=true';
+        } 
+        if ("state" in config) {
+            url= url + '&state=' + config.state;
+        } 
+        if("start_year" in config){
+            url= url + '&StartYear=' + config.start_year;
+        }
+        if("end_year" in config){
+            url= url + '&EndYear=' + config.end_year;
+        }
+        if("jurisdictions" in config){
+			url= url + '&jurisdictions[]=' + config.jurisdictions;
+        }
+        
+      
+        if(typeof callback != 'undefined'){
+            return Promise.resolve(exports.get_promise(url, params, "post"))
+            .nodeify(callback);
+        } else {
+            return exports.get_promise(url, params, "post")
+            .then(function(d){ 
+              return d;
+            });
+        }
+    },
+    
+    query_form: function (name, id, is_legacy, is_migrated, callback) {
+      
+        url = "api/v2.0/query/form/" + name + "/" + id + "?site_key=" + site_id;
+        if(is_legacy && is_legacy != '' && (is_legacy == true || is_legacy == "true" )){
+          url = url + '&is_lawatlas=true&use_lawatlas=true';
+        }
+        if(is_migrated && is_migrated != '' && (is_migrated == true || is_migrated == "true" )){
+          url = url + '&is_migrated_lawatlas_dataset=true';
+        }
+        //console.log("Form URL");
+        //console.log(url);
+        if(typeof callback != 'undefined'){
+            return Promise.resolve(exports.get_promise(url))
+            .nodeify(callback);
+        } else {
+            return exports.get_promise(url)
+            .then(function(d){ 
+              return d;
+            });
+      }
+    },
+    
+    display: function (name, display_id, preview_id, callback) {
+      
+      url = 'previewer/' + display_id + '/get?dataset=' + name + '&preview_id=' + preview_id;
+      if(typeof callback != 'undefined'){
+        return Promise.resolve(exports.get_promise(url))
+        .nodeify(callback);
+      } else {
+          return exports.get_promise(url)
+          //return exports.dataset_promise(name, display)
+            //.then(exports.parseResult)
+            //.then(null, this.retryErrors)
+            //.nodeify(callback)
+            .then(function(d){ 
+              //console.log(d);
+              return d;
+            });
+      }
+    },
+    
+    law: function (name, id, callback) {
+        url = 'lawGiver/' + name + '/' + id;
+        if(typeof callback != 'undefined'){
+            return Promise.resolve(exports.get_promise(url, {}, "post"))
+            .nodeify(callback);
+        } else {
+            return exports.get_promise(url, {}, "post")
+            .then(function(d){ 
+              return d;
+            });
+        }
+    },
+    
+    jurisdiction: function (id, callback) {
+      url = 'jurisdiction/getOne?id=' + id;
+      if(typeof callback != 'undefined'){
+        return Promise.resolve(exports.get_promise(url))
+        .nodeify(callback);
+      } else {
+          return exports.get_promise(url)
+          //return exports.dataset_promise(name, display)
+            //.then(exports.parseResult)
+            //.then(null, this.retryErrors)
+            //.nodeify(callback)
+            .then(function(d){ 
+              //console.log(d);
+              return d;
+            });
+      }
+    },
+    
+    jurisdiction_site_items: function (show_medals, callback) {
+        if(show_medals){
+            url = 'jurisdictionsiteitem/secondall';      
+        } else {
+            url = 'jurisdictionsiteitem/all';
+        }
+      
+      if(typeof callback != 'undefined'){
+        return Promise.resolve(exports.get_promise(url))
+        .nodeify(callback);
+      } else {
+          return exports.get_promise(url)
+          //return exports.dataset_promise(name, display)
+            //.then(exports.parseResult)
+            //.then(null, this.retryErrors)
+            //.nodeify(callback)
+            .then(function(d){ 
+              //console.log(d);
+              return d;
+            });
+      }
+    },
+    
+    page: function (name, callback) {
+      url = 'page/' + name + '/slug';
+      if(typeof callback != 'undefined'){
+        return Promise.resolve(exports.get_promise(url))
+        .nodeify(callback);
+      } else {
+          return exports.get_promise(url)
+          //return exports.dataset_promise(name, display)
+            //.then(exports.parseResult)
+            //.then(null, this.retryErrors)
+            //.nodeify(callback)
+            .then(function(d){ 
+              //console.log(d);
+              return d;
+            });
+      }
+    },
+    
+    taxonomies: function (root, callback) {
+      url = 'taxonomy/' + root + '/50status/items';
+      if(typeof callback != 'undefined'){
+        return Promise.resolve(exports.get_promise(url))
+        .nodeify(callback);
+      } else {
+          return exports.get_promise(url)
+          //return exports.dataset_promise(name, display)
+            //.then(exports.parseResult)
+            //.then(null, this.retryErrors)
+            //.nodeify(callback)
+            .then(function(d){ 
+              //console.log(d);
+              return d;
+            });
+      }
+    },
+    
+    taxonomy: function (id, callback) {
+      url = 'taxonomy/' + id + '/get';
+      if(typeof callback != 'undefined'){
+        return Promise.resolve(exports.get_promise(url))
+        .nodeify(callback);
+      } else {
+          return exports.get_promise(url)
+          //return exports.dataset_promise(name, display)
+            //.then(exports.parseResult)
+            //.then(null, this.retryErrors)
+            //.nodeify(callback)
+            .then(function(d){ 
+              //console.log(d);
+              return d;
+            });
+      }
+    },
+  
+    related: function (name, callback) {
+      url = 'taxonomy/' + name + '/related';
+      if(typeof callback != 'undefined'){
+        return Promise.resolve(exports.get_promise(url))
+        .nodeify(callback);
+      } else {
+          return exports.get_promise(url)
+          //return exports.dataset_promise(name, display)
+            //.then(exports.parseResult)
+            //.then(null, this.retryErrors)
+            //.nodeify(callback)
+            .then(function(d){ 
+              //console.log(d);
+              return d;
+            });
+      }
+    },
+    
+    
+    //Common functions
+    get_promise: function(url, params, method) {
+        if(typeof params == 'undefined' || params == null){
+            params = {};
+        }
+        if(method == "post"){
+            
+            var promise = new Promise(function (resolve, reject) {
+              client.post(url, params, function (err, res, body) {
+                if (err) reject(err);
+                else resolve(res);
+              });
+            });
+            
+        } else {
+        
+            var promise = new Promise(function (resolve, reject) {
+              client.get(url, function (err, res) {
+                if (err) reject(err);
+                else resolve(res);
+              });
+            });
+        }
      
-     var promise = new Promise(function (resolve, reject) {
-      client.get(url, function (err, res) {
-        if (err) reject(err);
-        else resolve(res);
-      });
-    });
-    
-    return Promise.resolve(promise);
-    //promise.then(this.onFulfilled, this.onRejected);
-      
-  },
-    
-  get_promise: function(url, name, display) {      
-     var promise = new Promise(function (resolve, reject) {
-      client.get(url, function (err, res) {
-        if (err) reject(err);
-        else resolve(res);
-      });
-    });
     return Promise.resolve(promise);
       
-  },
-  onFulfilled: function(result) {
+    },
+    onFulfilled: function(result) {
       //console.log("Promise resolved");
       //console.log(result);
       return result;
-  },
-  onRejected: function(result) {
+    },
+    onRejected: function(result) {
       console.error("Promise rejected");
       console.error(result);
       return null;
-  },
-    
-  /**
-  * Escape special characters in the given string of html.
-  *
-  * @param  {String} html
-  * @return {String}
-  */
-  escape: function(html) {
-    return String(html)
-      .replace(/&/g, '&amp;')
-      .replace(/"/g, '&quot;')
-      .replace(/'/g, '&#39;')
-      .replace(/</g, '&lt;')
-      .replace(/>/g, '&gt;');
-  },
-
-  /**
-   * Unescape special characters in the given string of html.
-   *
-   * @param  {String} html
-   * @return {String}
-   */
-  unescape: function(html) {
-    return String(html)
-      .replace(/&amp;/g, '&')
-      .replace(/&quot;/g, '"')
-      .replace(/&lt;/g, '<')
-      .replace(/&gt;/g, '>');
-  }
+    }
 };
 
-
-
-
-
-
-
-
-
-
-
-
-
-///NOtes and crap
-
-
-      //get questions_editor
-//      asyncTasks.push(function (callback) {
-//        client.get(url, function (err, res, body) {
-//            callback(body, null);
-//        });
-//      });
-//      async.parallel(asyncTasks, function (siteItem) {
-//        //res.send(siteItem);
-//        console.log(siteItem);
-//        return siteItem;
-//      });
-//      
-      //html = "<p>Dataset</p>";
-      //return String(html);
--- test/index.js
+++ test/index.js
@@ -1,12 +1,19 @@
 var should = require('chai').should(),
     expect = require('chai').expect,
     monqcle_api = require('../index'),
-    escape = monqcle_api.escape,
-    unescape = monqcle_api.unescape,
-    dataset = monqcle_api.dataset,
-    dataset_promise = monqcle_api.dataset_promise,
-    dataset_siteitem = monqcle_api.dataset_siteitem,
-    dataset_nodeify = monqcle_api.dataset_nodeify;
+    assets = monqcle_api.assets,
+    page = monqcle_api.page,
+    taxonomies = monqcle_api.taxonomies,
+    taxonomy = monqcle_api.taxonomy,
+    related = monqcle_api.related,
+    jurisdiction = monqcle_api.jurisdiction,
+    jurisdiction_site_items = monqcle_api.jurisdiction_site_items,
+    questions = monqcle_api.questions,
+    query = monqcle_api.query,
+    query_form = monqcle_api.query_form,
+    law = monqcle_api.law,
+    display = monqcle_api.display,
+    dataset = monqcle_api.dataset;
 
 //describe('#dataset', function() {
 //  it('prints dataset info', function() {
@@ -21,6 +28,118 @@ var should = require('chai').should(),
 //});
 
 describe('Dataset', function() {
+    
+  describe('#assets', function() {
+    this.timeout(10000);
+    it('should return assets without error', function(done) {
+      var a = assets();
+      a.then(function(){ done() });
+    });
+  });
+    
+  describe('#page', function() {
+    this.timeout(10000);
+    it('should return page without error', function(done) {
+      var p = page("pew");
+      p.then(function(){ done() });
+    });
+  });
+    
+  describe('#taxonomies', function() {
+    this.timeout(10000);
+    it('should return taxonomies without error', function(done) {
+      var t = taxonomies("58c2c1d8f7d1b60f0b334876");
+      t.then(function(){ done() });
+    });
+  });
+    
+  describe('#taxonomy', function() {
+    this.timeout(10000);
+    it('should return taxonomy without error', function(done) {
+      var t = taxonomy("58c2c1d8f7d1b60f0b334876");
+      t.then(function(){ done() });
+    });
+  });
+    
+  describe('#related', function() {
+    this.timeout(10000);
+    it('should return related taxonomy without error', function(done) {
+      var t = related("biosimilars-laws");
+      t.then(function(){ done() });
+    });
+  });
+    
+  describe('#jurisdiction', function() {
+    this.timeout(10000);
+    it('should return jurisdiction getOne without error', function(done) {
+      var j = jurisdiction("54f8b21c6eb07acf618b4619");
+      j.then(function(){ done() });
+    });
+  });
+    
+  describe('#jurisdiction_site_items', function() {
+    this.timeout(10000);
+    it('should return jurisdiction site items without error', function(done) {
+      var j = jurisdiction_site_items(true);
+      j.then(function(){ done() });
+    });
+  });
+    
+  describe('#questions', function() {
+    this.timeout(10000);
+    it('should return questions for a dataset without error', function(done) {
+      var q = questions("biosimilars-laws",false);
+      q.then(function(){ done() });
+    });
+  });
+  
+  describe('#query', function() {
+    this.timeout(10000);
+    it('should return dataset query without error', function(done) {
+      var q = query("biosimilars-laws",{start_year:"2002"}, {});
+      q.then(function(){ done() });
+    });
+  });
+    
+//  describe('#query_form', function() {
+//    this.timeout(20000);
+//    it('should return dataset query form without error', function(done) {
+//      var q = query_form("biosimilars-laws","last", false, false);
+//      q.then(function(){ done() });
+//    });
+//  });
+    
+  describe('#query_form', function() {
+      it('prints dataset query form using nodeify of promise', function() {
+          //dataset("biosimilars-laws","").should.equal('<p>Dataset</p>');
+          query_form("biosimilars-laws","last", false, false, function(form_info){
+
+              //console.log(dataset_info);
+              expect(form_info).to.not.be.undefined;
+              expect(form_info).to.not.be.null;
+              //expect(form_info).to.have.all.keys(['id', 'previews']);
+              //form_info['id'].should.equal("56f924f0d6c9e7cf2f422eb4");
+          });
+
+      });
+  });
+    
+  describe('#law', function() {
+    this.timeout(10000);
+    it('should return related law without error', function(done) {
+      var l = law("biosimilars-laws", "569fb88ff7d1b67c173348a0");
+      l.then(function(){ done() });
+    });
+  });
+    
+  describe('#display', function() {
+    this.timeout(10000);
+    it('should return display for a dataset without error', function(done) {
+      var d = display("biosimilars-laws","580a5c56f7d1b6ba7f09a19a", "57f66e68f7d1b66f0ca9bcbd");
+      d.then(function(){ done() });
+    });
+  });
+    
   describe('#dataset', function() {
     this.timeout(10000);
     it('should return dataset without error', function(done) {
@@ -29,18 +148,10 @@ describe('Dataset', function() {
     });
   });
     
-  describe('#siteitem', function() {
-    this.timeout(10000);
-    it('should return dataset_siteitem without error', function(done) {
-      var ds = dataset_siteitem("biosimilars-laws","");
-      ds.then(function(){ done() });
-    });
-  });
-    
-  describe('#siteitem_nodeify', function() {
+  describe('#dataset_nodeify', function() {
       it('prints dataset_siteitem info using nodeify of promise', function() {
           //dataset("biosimilars-laws","").should.equal('<p>Dataset</p>');
-          dataset_siteitem("biosimilars-laws","", function(dataset_info){
+          dataset("biosimilars-laws","", function(dataset_info){
 
               //console.log(dataset_info);
               expect(dataset_info).to.not.be.undefined;
@@ -50,67 +161,45 @@ describe('Dataset', function() {
           });
 
       });
-    });
-    
-  describe('#dataset_promise', function() {
-    this.timeout(10000);
-    it('should return dataset without error', function(done) {
-      var ds = dataset_promise("biosimilars-laws","");
-      ds.then(function(){ done() });
-    });
   });
+    
+//  
+//  describe('#siteitem_nodeify', function() {
+//      it('prints dataset_siteitem info using nodeify of promise', function() {
+//          //dataset("biosimilars-laws","").should.equal('<p>Dataset</p>');
+//          dataset_siteitem("biosimilars-laws","", function(dataset_info){
+//
+//              //console.log(dataset_info);
+//              expect(dataset_info).to.not.be.undefined;
+//              expect(dataset_info).to.not.be.null;
+//              expect(dataset_info).to.have.all.keys(['id', 'previews']);
+//              dataset_info['id'].should.equal("56f924f0d6c9e7cf2f422eb4");
+//          });
+//
+//      });
+//    });
+//    
+//  describe('#dataset_promise', function() {
+//    this.timeout(10000);
+//    it('should return dataset without error', function(done) {
+//      var ds = dataset_promise("biosimilars-laws","");
+//      ds.then(function(){ done() });
+//    });
+//  });
    
-  describe('#dataset_nodeify', function() {
-      it('prints dataset info using nodeify of promise', function() {
-          //dataset("biosimilars-laws","").should.equal('<p>Dataset</p>');
-          dataset_nodeify("biosimilars-laws","", function(dataset_info){
-
-              //console.log(dataset_info);
-              expect(dataset_info).to.not.be.undefined;
-              expect(dataset_info).to.not.be.null;
-              expect(dataset_info).to.have.all.keys(['id', 'previews']);
-              dataset_info['id'].should.equal("56f924f0d6c9e7cf2f422eb4");
-          });
-
-      });
-    });
+//  describe('#dataset_nodeify', function() {
+//      it('prints dataset info using nodeify of promise', function() {
+//          //dataset("biosimilars-laws","").should.equal('<p>Dataset</p>');
+//          dataset_nodeify("biosimilars-laws","", function(dataset_info){
+//
+//              //console.log(dataset_info);
+//              expect(dataset_info).to.not.be.undefined;
+//              expect(dataset_info).to.not.be.null;
+//              expect(dataset_info).to.have.all.keys(['id', 'previews']);
+//              dataset_info['id'].should.equal("56f924f0d6c9e7cf2f422eb4");
+//          });
+//
+//      });
+//    });
     
 });
-
-
-
-//describe('#escape', function() {
-//  it('converts & into &amp;', function() {
-//    escape('&').should.equal('&amp;');
-//  });
-//
-//  it('converts " into &quot;', function() {
-//    escape('"').should.equal('&quot;');
-//  });
-//
-//  it('converts < into &lt;', function() {
-//    escape('<').should.equal('&lt;');
-//  });
-//
-//  it('converts > into &gt;', function() {
-//    escape('>').should.equal('&gt;');
-//  });
-//});
-//
-//describe('#unescape', function() {
-//  it('converts &amp; into &', function() {
-//    unescape('&amp;').should.equal('&');
-//  });
-//
-//  it('converts &quot; into "', function() {
-//    unescape('&quot;').should.equal('"');
-//  });
-//
-//  it('converts &lt; into <', function() {
-//    unescape('&lt;').should.equal('<');
-//  });
-//
-//  it('converts &gt; into >', function() {
-//    unescape('&gt;').should.equal('>');
-//  });
-//});
