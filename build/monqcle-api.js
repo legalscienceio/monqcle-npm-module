8679914f5d814be2d9dcc34d76e90b23649010ee
Mark Rose <blackcorvidae@gmail.com>
2017-05-30 11:31:47 -0400

    build version is pretty buggy sigh

--- build/monqcle-api.js
+++ build/monqcle-api.js
@@ -1,17 +1,6 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var fs = require('fs');
-var config;
-try {
-    config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
-} catch (err) {
-    if (err.code === 'ENOENT') {
-      console.error('File not found!  You must add config.json to the root of this project.');
-    } else {
-      throw err;
-    }
-}
-var api_host = config.host;
-var site_id = config.site;
+var api_host = "http://test.monqcle.com/";
+var site_id = "56e805b9d6c9e75c1ac8cb12";
 var _ = require('lodash');
 request = require('request-json');
 var client = request.createClient(api_host);
